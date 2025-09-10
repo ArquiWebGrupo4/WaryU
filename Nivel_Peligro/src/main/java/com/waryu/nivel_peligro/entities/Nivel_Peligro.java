84b83f1f5ff25e86a55eeb3274ce2c9485909179
AndreyC2808 <andreycem04@gmail.com>
2025-09-10 11:18:21 -0500

    Nivel de Peligro

---
+++ Nivel_Peligro/src/main/java/com/waryu/nivel_peligro/entities/Nivel_Peligro.java
@@ -0,0 +1,36 @@
+package com.waryu.nivel_peligro.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Nivel_Peligro")
+public class Nivel_Peligro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID_nivel;
+    @Column(name ="Nivel", nullable=false)
+    private int nivel;
+
+    public Nivel_Peligro() {}
+
+    public Nivel_Peligro(int ID_nivel, int nivel) {
+        this.ID_nivel = ID_nivel;
+        this.nivel = nivel;
+    }
+
+    public int getID_nivel() {
+        return ID_nivel;
+    }
+
+    public void setID_nivel(int ID_nivel) {
+        this.ID_nivel = ID_nivel;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+}
