5afaa64f288ec6477e993ecf8bc13af21c8e6d35
DanproTDT <162157821+DanproTDT@users.noreply.github.com>
2025-09-20 22:35:58 -0500

    feat(security): Implementar interaccion con APIs desde Swagger

--- waryu/src/main/java/com/example/waryu/Securities/WebSecurityConfig.java
+++ waryu/src/main/java/com/example/waryu/Securities/WebSecurityConfig.java
@@ -61,7 +61,11 @@ public class WebSecurityConfig {
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/login",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
